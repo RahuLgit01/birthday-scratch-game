93a32eeb5656c4b52b51a99bbb53d060d99d5867
Rahul <110444562+RahuLgit01@users.noreply.github.com>
2026-06-08 14:17:42 +0530

    Add files via upload

---
+++ configuration.js
@@ -0,0 +1,39 @@
+var configuration = {
+    games: [
+        {
+            imagePath: "images/japans.jpeg",
+            backgroundImagePath: "images/light-blue-opacity.png",
+            message: "Am I right your favourite destination country is Japan?."
+        },
+        {
+            imagePath: "images/Spaghetti.jpeg",
+            backgroundImagePath: "images/light-blue-opacity.png",
+            message: "Hmmm..., Your Favourite food is Spaghetti/Pasta..??"
+        },
+        {
+            imagePath: "images/lakshitars.jpeg",
+            backgroundImagePath: "images/orange-opacity.png",
+            message: "WoW,<br>Tenu suit sute krda.. <br>Yet, only one was left to win..."
+        },
+        {
+            imagePath: "images/lakand.jpeg",
+            backgroundImagePath: "images/light-blue-opacity.png",
+            message: "Itni jaldi kya hai thoda tang krlu,<br> Apni best friend dekhti jaa hahahaha!"
+        },
+        {
+            imagePath: "images/kittens.jpeg",
+            backgroundImagePath: "images/orange-opacity.png",
+            message: "your fav kitten <br>this cute ball of fur can wait"
+        },
+        {
+            imagePath: "images/cakes.jpeg",
+            backgroundImagePath: "images/light-blue-opacity.png",
+            class: "winner",
+            message: "" +
+                "Congratulations!! " +
+                "You are the lucky winner Maharani! \ you won birthday treat from me..." +
+                "<br> <img src='images/leo.gif'>" +
+            ""
+        }
+    ]
+};
